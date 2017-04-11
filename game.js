7994e9cdc664aaeffcd4f2abd711b943dd86e63b
Sam Heckle <samheckle@yahoo.com>
2017-04-11 11:30:36 -0400

    added collisions

--- game.js
+++ game.js
@@ -22,28 +22,28 @@ document.addEventListener("keyup", keysUp, false);
 // when key is pressed down, move
 function keysDown(e) {
 	// right arrow key
-	if(e.keyCode == 39){
+	if (e.keyCode == 39) {
 		moveRight = true;
 	}
 	// left arrow key
-	else if(e.keyCode == 37){
+	else if (e.keyCode == 37) {
 		moveLeft = true;
 	}
 	// space bar to start
-	else if(e.keyCode == 32 && gameOver){
+	else if (e.keyCode == 32 && gameOver) {
 		// call playAgain to start the game
 		playAgain();
 	}
 }
 // if a key is released then stop movement
 function keysUp(e) {
-	if(e.keyCode == 39){
+	if (e.keyCode == 39) {
 		moveRight = false;
 	}
-	else if(e.keyCode == 37){
+	else if (e.keyCode == 37) {
 		moveLeft = false;
 	}
-	
+
 }
 
 // set up the player
@@ -52,28 +52,28 @@ var player = {
 	size: 35,
 	color: "#E887E5",
 	// put in middle of screen
-	x: (canvas.width -30)/ 2,
+	x: (canvas.width - 30) / 2,
 	y: canvas.height - 30
-	
+
 };
 
 // set up falling objects
 var fallingCircles = [];
 function initializeCircle() {
 	fallingCircles.push({
-		x:Math.random()*canvas.width,
-		y:0,
-		speed: Math.floor( (Math.random()+1)*3 ),
+		x: Math.random() * canvas.width,
+		y: 0,
+		speed: Math.floor((Math.random() + 1) * 3),
 		color: ["black", "purple", "white"]
 	});
 }
-for(var i = 0; i < numCircles; i++) {
+for (var i = 0; i < numCircles; i++) {
 	initializeCircle();
 }
 
 
 function drawCircles() {
-	for(var i = 0; i < fallingCircles.length; i++) {
+	for (var i = 0; i < fallingCircles.length; i++) {
 		ctx.beginPath();
 		ctx.arc(fallingCircles[i].x, fallingCircles[i].y, 5, 0, 2 * Math.PI);
 		ctx.fillStyle = fallingCircles[i].color[Math.floor(Math.random() * 3)];
@@ -91,33 +91,45 @@ function drawPlayer() {
 	ctx.closePath();
 }
 
+function checkCollisions() {
+	for (var i = 0; i < fallingCircles.length; i++) {
+		if (player.x < fallingCircles[i].x + 5 &&
+			player.x + player.size > fallingCircles[i].x &&
+			player.y < fallingCircles[i].y + 5 &&
+			player.size + player.y > fallingCircles[i].y) {
+			score += 10;
+			fallingCircles.splice(i, 1);
+		}
+
+	}
+}
+
 // update function
 function Update() {
 
 	// circles
-	for(var i = 0; i < fallingCircles.length; i++) {
+	for (var i = 0; i < fallingCircles.length; i++) {
 		fallingCircles[i].y = fallingCircles[i].y + fallingCircles[i].speed;
 
-	if (fallingCircles[i].y == Math.floor(canvas.height/2)) {
+		if (fallingCircles[i].y == Math.floor(canvas.height / 2)) {
 			initializeCircle();
-	} else if (fallingCircles[i].y > canvas.height) {
-			score+=10;
+		} else if (fallingCircles[i].y > canvas.height) {
 			fallingCircles.splice(i, 1);
 		}
 	}
 	// player
-	if(moveLeft && player.x > 0){
+	if (moveLeft && player.x > 0) {
 		player.x -= 7;
 	}
-	if(moveRight && player.x + player.size < canvas.width) {
+	if (moveRight && player.x + player.size < canvas.width) {
 		player.x += 7;
 	}
 
-
+	checkCollisions();
 
 }
 // end of game 
-function gamesOver(){
+function gamesOver() {
 	gameOver = true;
 }
 
@@ -130,10 +142,10 @@ function playAgain() {
 	lives = 3;
 
 }
-function draw(){
+function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	if(!gameOver){
-		
+	if (!gameOver) {
+
 		// background color
 		ctx.fillStyle = "#B3B3B3";
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -141,27 +153,27 @@ function draw(){
 		drawPlayer();
 		drawCircles();
 		Update();
-			
+
 		// score
 		ctx.fillStyle = "black";
 		ctx.font = "15px Verdana";
 		ctx.textAlign = "left";
 		ctx.fillText("Score: " + score, 10, 25);
-	
+
 		// lives
 		ctx.textAlign = "right";
 		ctx.fillText("Lives: " + lives, 500, 25);
 	}
-	else{
+	else {
 		ctx.fillStyle = "black";
 		ctx.font = "25px Verdana";
 		ctx.textAlign = "center";
-		ctx.fillText("GAME OVER!", canvas.width/2, 175);
-		
+		ctx.fillText("GAME OVER!", canvas.width / 2, 175);
+
 		ctx.font = "20px Verdana";
-		ctx.fillText("PRESS SPACE TO PLAY", canvas.width/2, 475);
-		
-		ctx.fillText("SCORE: " + score, canvas.width/2, 230);
+		ctx.fillText("PRESS SPACE TO PLAY", canvas.width / 2, 475);
+
+		ctx.fillText("SCORE: " + score, canvas.width / 2, 230);
 	}
 	document.getElementById("level").innerHTML = "Level: " + level;
 	requestAnimationFrame(draw);
